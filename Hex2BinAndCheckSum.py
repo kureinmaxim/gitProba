d710094748d0da762424b734c178b0f01b64ba7f
MXM <kurein.maxim@gmail.com>
2024-12-26 10:57:04 +0300

    Исправлен файл Hex2BinAndCheckSum.py

--- Hex2BinAndCheckSum.py
+++ Hex2BinAndCheckSum.py
@@ -3,14 +3,15 @@ import intelhex
 def calculate_checksum(data, start_address):
     checksum = 0
     for i in range(start_address, len(data), 2):
-        word = (data[i + 1] << 8) + data[i]
+       # word = (data[i + 1] << 8) + data[i]
+        word = (data[i] << 8) + data[i + 1]
         checksum = (checksum + word) & 0xFFFF
     return checksum
 
 def main():
-    input_hex_file = "C:/Project/ProjectCodeMaster/PriborTL7/PriborTL7/Debug/Exe/PriborTL7.HEX"
-    output_bin_file = "C:/Project/ProjectCodeMaster/PriborTL7/PriborTL7/Debug/Exe/PriborTL7.bin"
-    output_bin_file_cs = "C:/Project/ProjectCodeMaster/PriborTL7/PriborTL7/Debug/Exe/PriborTL7_cs.bin"
+    input_hex_file = "C:/Project/Project_CodeMaster/PriborTL7/PriborTL7/Debug/Exe/PriborTL7.HEX"
+    output_bin_file = "C:/Project/Project_CodeMaster/PriborTL7/PriborTL7/Debug/Exe/PriborTL7.bin"
+    output_bin_file_cs = "C:/Project/Project_CodeMaster/PriborTL7/PriborTL7/Debug/Exe/PriborTL7_cs.bin"
     start_address = 0x2000
     bin_size = 32768
     bin_size2cs = 32766
@@ -28,7 +29,9 @@ def main():
 
     # Calculate the checksum
     checksum = calculate_checksum(bin_data_cs, start_address)
-    checksum_bytes = checksum.to_bytes(2, byteorder='little')
+    #checksum_bytes = checksum.to_bytes(2, byteorder='little')
+    checksum_bytes = checksum.to_bytes(2, byteorder='big')
+    print("Checksum bytes (hex):", ' '.join(f'{b:02x}' for b in checksum_bytes))
 
     # Write the binary data to the second file with checksum
     with open(output_bin_file_cs, 'wb') as f:
